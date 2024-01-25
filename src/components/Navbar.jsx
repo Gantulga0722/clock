36e4f8fdb2672d3539d408a2c96820fe223fcfbd
Gantulga0722 <mgtulga22@gmail.com>
2024-01-25 12:22:46 +0800

    From Pinecone

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Router } from "next/router";
+import { useRouter } from "next/router";
+
+export const Navbar = () => {
+  const router = useRouter();
+  return (
+    <div className="flex justify-between">
+      <button
+        className="flex bg-red-300"
+        onClick={() => router.push("/LocalClock")}
+      >
+        Clock
+      </button>
+      <button
+        className="flex bg-red-300"
+        onClick={() => router.push("/StopWatch")}
+      >
+        Stop Watch
+      </button>
+      <button className="flex bg-red-300" onClick={() => router.push("/Timer")}>
+        Timer
+      </button>
+    </div>
+  );
+};
